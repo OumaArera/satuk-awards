e6dceb7a2930724e28c9d2f420b11ae25ac850e9
OumaArera <johnouma999@gmail.com>
2024-10-26 16:35:38 +0300

    feat: complete

--- src/Vote.jsx
+++ src/Vote.jsx
@@ -242,7 +242,7 @@ const Vote = () => {
   const [isVotingOpen, setIsVotingOpen] = useState(true); // to track if voting is open
   const siteKey = process.env.REACT_APP_RECAPTCHA_SITE_KEY;
 
-  const votingStartDate = new Date("2024-10-31T00:00:00Z");
+  const votingStartDate = new Date("2024-10-26T00:00:00Z");
   const votingEndDate = new Date("2024-11-08T00:00:00Z");
 
   useEffect(() => {
