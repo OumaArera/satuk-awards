1972a672f3e8936b2d83eee18b34e5c434d09ab4
OumaArera <johnouma999@gmail.com>
2024-10-31 08:59:12 +0300

    feat: complete

--- src/Vote.jsx
+++ src/Vote.jsx
@@ -21,209 +21,209 @@ const votingDetails=[
   },
   {
     categoryId: 3,
-    categoryName: "The President's Commendation Award",
+    categoryName: "The Student Leader of the Year Award",
     candidates: [
-      {name: "Test1", id: 7},
-      {name: "Test2", id: 8},
-      {name: "Test3", id: 9}
+      {name: "Yvonne Nyanchera", id: 7},
+      {name: "Peter Marite", id: 8},
+      {name: "Peter Kanyua", id: 9}
     ]
   },
   {
     categoryId: 4,
     categoryName: "The Upcoming Student Leader of the Year Award",
     candidates: [
-      {name: "Test1", id: 10},
-      {name: "Test2", id: 11},
-      {name: "Test3", id: 12}
+      {name: "Sherline Wambui", id: 10},
+      {name: "Lawrence Ojiambo", id: 11},
+      {name: "Adam Mwinzi", id: 12}
     ]
   },
   {
     categoryId: 5,
     categoryName: "The Most Influential Student of the Year Award",
     candidates: [
-      {name: "Test1", id: 13},
-      {name: "Test2", id: 14},
-      {name: "Test3", id: 15}
+      {name: "Julias Narikae", id: 13},
+      {name: "Evance Ouma", id: 14},
+      {name: "Charles Bwibo", id: 15}
     ]
   },
   {
     categoryId: 6,
     categoryName: "The Student's Affairs Advocate of the Year Award",
     candidates: [
-      {name: "Test1", id: 16},
-      {name: "Test2", id: 17},
-      {name: "Test3", id: 18}
+      {name: "James Kinuthia", id: 16},
+      {name: "Stanley Motinda", id: 17},
+      {name: "Martin Nyaga", id: 18}
     ]
   },
   {
     categoryId: 7,
     categoryName: "The Blogger of the Year Award",
     candidates: [
-      {name: "Test1", id: 19},
-      {name: "Test2", id: 20},
-      {name: "Test3", id: 21}
+      {name: "Mercyline Namasaka", id: 19},
+      {name: "Austine Wambingwa", id: 20},
+      {name: "Lucky Jones", id: 21}
     ]
   },
   {
     categoryId: 8,
     categoryName: "The Faculty of the Year Award",
     candidates: [
-      {name: "Test1", id: 22},
-      {name: "Test2", id: 23},
-      {name: "Test3", id: 24}
+      {name: "FSST", id: 22},
+      {name: "FEBE", id: 23},
+      {name: "FAST", id: 24}
     ]
   },
   {
     categoryId: 9,
     categoryName: "The Content Creator of the Year Award",
     candidates: [
-      {name: "Test1", id: 25},
-      {name: "Test2", id: 26},
-      {name: "Test3", id: 27}
+      {name: "Comrade Premium TUK", id: 25},
+      {name: "Burudani 14", id: 26},
+      {name: "TUK Comrade", id: 27}
     ]
   },
   {
     categoryId: 10,
     categoryName: "The Club and/or Society of the Year Award",
     candidates: [
-      {name: "Test1", id: 28},
-      {name: "Test2", id: 29},
-      {name: "Test3", id: 30}
+      {name: "Drama", id: 28},
+      {name: "French Club", id: 29},
+      {name: "Biochemestry and Biotechnology Students Association", id: 30}
     ]
   },
   {
     categoryId: 11,
     categoryName: "The Humanitarian of the Year Award",
     candidates: [
-      {name: "Test1", id: 31},
-      {name: "Test2", id: 32},
-      {name: "Test3", id: 33}
+      {name: "Daltone Ogonda", id: 31},
+      {name: "Felicity Orifah", id: 32},
+      {name: "Elphas Aduwa", id: 33}
     ]
   },
   {
     categoryId: 12,
     categoryName: "The Most Innovative and Creative Student of the Year Award",
     candidates: [
-      {name: "Test1", id: 34},
-      {name: "Test2", id: 35},
-      {name: "Test3", id: 36}
+      {name: "Kevin Kariuki", id: 34},
+      {name: "David Onyango", id: 35},
+      {name: "Francis Mwendwa", id: 36}
     ]
   },
   {
     categoryId: 13,
     categoryName: "The Entrepreneurial Student of the Year Award",
     candidates: [
-      {name: "Test1", id: 37},
-      {name: "Test2", id: 38},
-      {name: "Test3", id: 39}
+      {name: "Martha Muraya", id: 37},
+      {name: "Tirati Malei", id: 38},
+      {name: "Dinah Mwikali", id: 39}
     ]
   },
   {
     categoryId: 25,
     categoryName: "The Graphic Designer of the Year Award",
     candidates: [
-      {name: "Test1", id: 73},
-      {name: "Test2", id: 74},
-      {name: "Test3", id: 75}
+      {name: "Evans Kimutai", id: 73},
+      {name: "Peter Muchina", id: 74},
+      {name: "Isaac Kiarita", id: 75}
     ]
   },
   {
     categoryId: 14,
     categoryName: "The Poet and Song Writer of the Year Award",
     candidates: [
-      {name: "Test1", id: 40},
-      {name: "Test2", id: 41},
-      {name: "Test3", id: 42}
+      {name: "Timothy Olumunyak", id: 40},
+      {name: "Ray Junior Orek", id: 41},
+      {name: "Yovan Manoa", id: 42}
     ]
   },
   {
     categoryId: 15,
     categoryName: "The Photographer of the Year Award",
     candidates: [
-      {name: "Test1", id: 43},
-      {name: "Test2", id: 44},
-      {name: "Test3", id: 45}
+      {name: "White Shadow Photography", id: 43},
+      {name: "Kenyanza Art Photography", id: 44},
+      {name: "Crews Photography", id: 45}
     ]
   },
   {
     categoryId: 16,
     categoryName: "The Dance Crew of the Year Award",
     candidates: [
-      {name: "Test1", id: 46},
-      {name: "Test2", id: 47},
-      {name: "Test3", id: 48}
+      {name: "Creative Ministry", id: 46},
+      {name: "Catalan Dancers", id: 47},
+      {name: "Dolores Tempora Temp", id: 48}
     ]
   },
   {
     categoryId: 17,
     categoryName: "The Service Provider of the Year Award",
     candidates: [
-      {name: "Test1", id: 49},
-      {name: "Test2", id: 50},
-      {name: "Test3", id: 51}
+      {name: "Tecla Barasa", id: 49},
+      {name: "Omondi Rolex", id: 50},
+      {name: "Adika Ladslaus", id: 51}
     ]
   },
   {
     categoryId: 18,
     categoryName: "The Environment Advocate of the Year Award",
     candidates: [
-      {name: "Test1", id: 52},
-      {name: "Test2", id: 53},
-      {name: "Test3", id: 54}
+      {name: "Yvette Kenyatta", id: 52},
+      {name: "Moses Okal", id: 53},
+      {name: "Michael Onduto", id: 54}
     ]
   },
   {
     categoryId: 19,
     categoryName: "The PWDs Advocate of the Year Award",
     candidates: [
-      {name: "Test1", id: 55},
-      {name: "Test2", id: 56},
-      {name: "Test3", id: 57}
+      {name: "Leila Abdi", id: 55},
+      {name: "Albert Ooko", id: 56},
+      {name: "Asumwa Amwilu", id: 57}
     ]
   },
   {
     categoryId: 20,
     categoryName: "The Commitment to Service Award",
     candidates: [
-      {name: "Test1", id: 58},
-      {name: "Test2", id: 59},
-      {name: "Test3", id: 60}
+      {name: "Damaris Njeri", id: 58},
+      {name: "Lenox Onyango", id: 59},
+      {name: "Maurice Cornel", id: 60}
     ]
   },
   {
     categoryId: 21,
     categoryName: "Indoor Games, Sports Team of the Year Award",
     candidates: [
-      {name: "Test1", id: 61},
-      {name: "Test2", id: 62},
-      {name: "Test3", id: 63}
+      {name: "Taekwondo", id: 61},
+      {name: "Badminton", id: 62},
+      {name: "Chess", id: 63}
     ]
   },
   {
     categoryId: 22,
     categoryName: "Outdoor Games, Sports Team of the Year Award",
     candidates: [
-      {name: "Test1", id: 64},
-      {name: "Test2", id: 65},
-      {name: "Test3", id: 66}
+      {name: "Basketball Ladies", id: 64},
+      {name: "TUK Hammers", id: 65},
+      {name: "Hockey", id: 66}
     ]
   },
   {
     categoryId: 23,
     categoryName: "Indoor Games, Sports Person of the Year Award",
     candidates: [
-      {name: "Test1", id: 67},
-      {name: "Test2", id: 68},
-      {name: "Test3", id: 69}
+      {name: "Lawrence Osuru", id: 67},
+      {name: "Ian Brian Ouma", id: 68},
+      {name: "Martin Abwao", id: 69}
     ]
   },
   {
     categoryId: 24,
     categoryName: "Outdoor Games, Sports Person of the Year Award",
     candidates: [
-      {name: "Test1", id: 70},
-      {name: "Test2", id: 71},
-      {name: "Test3", id: 72}
+      {name: "Mitchelle Ann Odhiambo", id: 70},
+      {name: "Emmanuel Palimwacha", id: 71},
+      {name: "Jasper Ombaso", id: 72}
     ]
   }
 ]
@@ -237,10 +237,10 @@ const Vote = () => {
   const [errorMessage, setErrorMessage] = useState('');
   const [success, setSuccess] = useState('');
   const [captchaToken, setCaptchaToken] = useState(null);
-  const [isVotingOpen, setIsVotingOpen] = useState(true); // to track if voting is open
+  const [isVotingOpen, setIsVotingOpen] = useState(true);
   const siteKey = process.env.REACT_APP_RECAPTCHA_SITE_KEY;
 
-  const votingStartDate = new Date("2024-10-31T21:00:00Z");
+  const votingStartDate = new Date("2024-10-29T21:00:00Z");
   const votingEndDate = new Date("2024-11-08T21:00:00Z");
 
   useEffect(() => {
@@ -248,6 +248,13 @@ const Vote = () => {
     if (now < votingStartDate || now > votingEndDate) {
       setIsVotingOpen(false);
     }
+
+    // Automatically set the first candidates for categories 1 and 2
+    setSelectedCandidates((prev) => ({
+      ...prev,
+      1: votingDetails[0].candidates[0].id, // First candidate in the first category
+      2: votingDetails[1].candidates[0].id, // First candidate in the second category
+    }));
   }, []);
 
   const validateEmail = (email) => {
@@ -293,7 +300,6 @@ const Vote = () => {
       captchaToken: captchaToken,
     };
 
-
     try {
       const response = await fetch('https://satuk.onrender.com/users/vote', {
         method: 'POST',
@@ -335,7 +341,7 @@ const Vote = () => {
               />
             </div>
 
-            {votingDetails.map((category) => (
+            {votingDetails.slice(2).map((category) => (
               <div key={category.categoryId} className="mb-4">
                 <h2 className="font-semibold mb-2">{category.categoryName}</h2>
                 {category.candidates.map((candidate) => (
@@ -383,5 +389,6 @@ const Vote = () => {
 };
 
 export default Vote;
+
 
 
